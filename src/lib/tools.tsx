37067bc01cef07bb5145dccada2437c436f7a554
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2023-12-29 11:42:10 -0300

    added description and icon background

--- src/lib/tools.tsx
+++ src/lib/tools.tsx
@@ -13,73 +13,88 @@ export interface IToolsProps {
   label: string;
   description?: string;
   icon: {
-    color: string;
     image: LucideIcon;
+    color: string;
+    background: string;
   };
   href: string;
   isTool?: boolean;
 }
 export const tools: IToolsProps[] = [
   {
-    icon: {
-      color: "text-sky-500",
-      image: LayoutDashboard
-    },
     label: "Dashboard",
+    description: "Our most advanced conversation model.",
+    icon: {
+      image: LayoutDashboard,
+      color: "text-sky-500",
+      background:"bg-sky-800/20",
+    },
     href: "/dashboard",
   },
   {
-    icon: {
-      color: "text-violet-500",
-      image: MessageSquare
-    },
     label: "Conversation",
+    description: "Our most advanced conversation model.",
+    icon: {
+      image: MessageSquare,
+      color: "text-violet-500",
+      background:"bg-violet-200",
+    },
     href: "/dashboard/ai/conversation",
     isTool: true,
   },
   {
-    icon: {
-      color: "text-pink-500",
-      image: ImageIcon
-    },
     label: "Image Generation",
+    description: "Our most advanced Image generate model.",
+    icon: {
+      image: ImageIcon,
+      color: "text-pink-500",
+      background:"bg-pink-200",
+    },
     href: "/dashboard/ai/image",
     isTool: true,
   },
   {
-    icon: {
-      color: "text-orange-500",
-      image: VideoIcon
-    },
     label: "Video Generation",
+    description: "Our most advanced video generate model.",
+    icon: {
+      image: VideoIcon,
+      color: "text-orange-500",
+      background:"bg-orange-200",
+    },
     href: "/dashboard/ai/video",
     isTool: true,
   },
   {
+    label: "Music Generation",
+    description: "Our most advanced music generate model.",
     icon: {
       image: Music,
       color: "text-emerald-500",
+      background:"bg-emerald-200",
     },
-    label: "Music Generation",
     href: "/dashboard/ai/music",
     isTool: true,
   },
   {
+    label: "Code Generation",
+    description: "Our most advanced code generate model.",
     icon: {
       image: Code,
       color: "text-green-500",
+      background:"bg-green-200",
     },
-    label: "Code Generation",
     href: "/dashboard/ai/code",
 
     isTool: true,
   },
   {
+    label: "Settings",
+    description: "Our most advanced settings generate model.",
     icon: {
       image: Settings,
       color: "text-white",
+      background:"bg-zinc-200",
     },
-    label: "Settings",
     href: "/dashboard/settings",
 
   },
