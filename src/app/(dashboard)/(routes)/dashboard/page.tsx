58d4ab2e8b976f743436abc0ebed9e096e469d5e
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2023-12-28 22:01:13 -0300

    init

---
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const DashboardPage = () => {
+  return <p>Dashboard Page (Protected)</p>;
+};
+
+export default DashboardPage;
